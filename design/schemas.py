6ba7929d70930264948f886833d6cf71c3ea16fb
gizm00 <4462255+gizm00@users.noreply.github.com>
2022-11-06 17:31:15 -0800

    updates for json schema

--- design/schemas.py
+++ design/schemas.py
@@ -2,10 +2,74 @@
 from pyspark.sql.types import StructType, LongType, StringType, ArrayType, StructField
 
 
-StructType(
+spark_schema = StructType(
     [StructField("count", LongType(),True), 
     StructField("description", StringType(),True),
     StructField("user", StringType(),False),
     StructField("img_files", ArrayType(StringType(),True),True),
     StructField("location", ArrayType(StringType(),True),True)]
 )
+
+initial_json_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "array",
+    "items": [{
+        "type": "object",
+        "properties": {
+        "user": {"type": "string"},
+        "location": {
+            "type": "array",
+            "items": [
+                {"type": "string"},
+                {"type": "string"}
+            ]
+        },
+        "img_files": {
+            "type": "array",
+            "items": [{"type": "string"}]
+        },
+        "description": {
+            "type": "string"
+        },
+        "count": {
+            "type": "integer"
+        }
+        },
+        "required": [
+        "user",
+        "location"
+        ]
+    }]
+}
+
+updated_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "array",
+    "items": [{
+        "type": "object",
+        "properties": {
+        "user": {"type": "string"},
+        "location": {
+            "type": "array",
+            "minItems":2,
+            "items": [
+                {"type": "string"}
+            ]
+        },
+        "img_files": {
+            "type": "array",
+            "items": [{"type": "string"}]
+        },
+        "description": {
+            "type": "string"
+        },
+        "count": {
+            "type": "integer"
+        }
+        },
+        "required": [
+        "user",
+        "location"
+        ]
+    }]
+}
